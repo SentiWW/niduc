5d277410ff965b9c163d03e2a28a412a2a87ee48
NotImplementedUsername <105523062+NotImplementedUsername@users.noreply.github.com>
2023-03-30 21:53:28 +0200

    Example of message flow
    Details:
    - 1-bit message
    - Repetition code, length 3
    - BSC channel

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+import komm
+
+
 file = open("test-data.txt")
 
 for line in file:
     for char in line.strip():
         char_bytes = bytes(char, "utf-8")
         print(char_bytes.hex())
+
+
+# code length
+n = 3
+
+# channel crossover probability
+p = 0.1
+
+# message
+msg = [1]
+
+# coder
+code = komm.RepetitionCode(n)
+
+# channel
+bsc = komm.BinarySymmetricChannel(p)
+
+
+coded = code.encode(msg)                # coding message
+y = bsc(coded)                          # "sending" message
+decoded = code.decode(y)                # decoding message
+
+print(decoded)
