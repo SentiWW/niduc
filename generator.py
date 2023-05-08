023f07132936347cb20f8e1dd41edaa167e37b03
Pi0trek8 <piotrekszczypior1@gmail.com>
2023-05-08 23:09:09 +0200

    data generator using faker package

---
+++ generator.py
@@ -0,0 +1,13 @@
+import faker
+from faker import Faker
+
+def create_data(file_name: str, words_per_sentence=10, number_of_sentences=10):
+    fake = Faker()
+    words = fake.sentence(words_per_sentence, True)
+    for i in range(number_of_sentences-1): 
+        words += " " + fake.sentence(words_per_sentence, True)
+
+    file = open(file_name, 'w')
+    file.write(words)
+    file.close
+
